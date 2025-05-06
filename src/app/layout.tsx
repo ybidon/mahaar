86beb2b07d4383ca318cc05a8120d5461555e85d
Your Name <your.email@example.com>
2025-05-06 16:34:26 +0400

    Add Twitter conversion tracking script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Link from "next/link";
 import Navbar from "@/components/Navbar";
 import FooterNavbar from '@/components/FooterNavbar';
+import Script from 'next/script';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -31,6 +32,14 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="icon" href="/pearl.svg" type="image/svg+xml" />
+        <Script id="twitter-conversion-tracking" strategy="afterInteractive">
+          {`
+            !function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);
+            },s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='https://static.ads-twitter.com/uwt.js',
+            a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))}(window,document,'script');
+            twq('config','powcq');
+          `}
+        </Script>
       </head>
       <body className={inter.className}>
         <Navbar />
